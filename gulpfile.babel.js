1a604e84212df087f2edee5688502ca872a00527
Tim Malabuyo <tmalabuyo@google.com>
2016-10-08 14:04:53 +0100

    Sets up basic dependencies + ES6

---
+++ gulpfile.babel.js
@@ -0,0 +1,7 @@
+import del from 'del';
+import gulp from 'gulp';
+import changed from 'gulp-changed';
+import fileInclude from 'gulp-file-include';
+import sass from 'gulp-sass';
+import watch from 'gulp-watch';
+import webserver from 'gulp-webserver';
